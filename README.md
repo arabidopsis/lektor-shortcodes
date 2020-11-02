ec2cad3b5420687cc6e653841e604de67d5132b4
Ian Castleden <ian.castleden@gmail.com>
2020-11-02 09:02:14 +0800

    added README text

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # shortcodes
 
-This is where a description of your plugin goes.
-Provide usage instructions here.
+Add Hugo style "shortcodes" to lektor e.g.
+
+`{{ youtube rXZGDADbdad width=50% height=300 position=left }}`
+
+This requires a template `youtube.html` in the `shortcodes` directory
+of your templates folder. Positional arguments will be passed to
+the template in the variable `args`.
+
+Currently no argument checking is done so the template should be
+very defensive and have defaults for all the keyword arguments.
+
+For example for the `shortcodes/youtube.html` you might have:
+
+```html
+
+<iframe
+    {% if class %} class="{{class}}"{% endif %}
+    {% if position %} style="float:{{position}}"{% endif %}
+    width="{{width|default(560)}}"
+    height="{{height|default(315)}}"
+    src="https://www.youtube.com/embed/{{args[0]}}"
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen>
+</iframe>
+
+```
+
+## Installation
+
+place this package in the directory packages maybe with
+
+```bash
+git submodule add https://github.com/arabidopsis/lektor-shortcodes.git packages/shortcodes
+lektor plugins reinstall
+```
--- lektor_shortcodes.py
+++ lektor_shortcodes.py
@@ -1,12 +1,79 @@
 # -*- coding: utf-8 -*-
+import re
 from lektor.pluginsystem import Plugin
+from lektor.context import get_ctx
+from jinja2 import TemplateNotFound
+
+
+SHORTCODE = re.compile(r"{{\s*(.*?)\s*}}")
+
+QUOTES = re.compile(r'["]([^"].*?)["]')
+
+
+def parse_args(s):
+    d = {}
+
+    def f(m):
+        i = len(d)
+        k = f"#######{i}#######"
+        d[k] = m.group(1)
+        return k
+
+    def fix_val(v):
+        if v in d:
+            return d[v]
+        if v.isdigit():
+            return int(v)
+        return v
+
+    s = QUOTES.sub(f, s)
+    args = s.split()
+    kwargs = dict(a.split("=", 1) for a in args if "=" in a)
+    args = [a for a in args if "=" not in a]
+    kwargs = {k: fix_val(v) for k, v in kwargs.items()}
+    args = [fix_val(v) for v in args]
+    return args, kwargs
+
+
+def render(cmd, args, kwargs):
+    try:
+        ctx = get_ctx()
+        if ctx is None:
+            return f"[no build context for {cmd}]"
+        if args:
+            kwargs["args"] = args
+        return ctx.env.render_template(
+            [
+                f"shortcodes/{cmd}.html",
+                # "blocks/default.html",
+            ],
+            pad=ctx.pad,
+            this=None,
+            values=kwargs,
+        )
+    except TemplateNotFound:
+        return f'[could not find shortcode "{cmd}.html" template]'
+
+
+class ShortcodesMixin:
+    name = "Markdown Shortcodes"
+    description = "Embeds shortcodes in Markdown."
+
+    def paragraph(self, text):
+        def shortcode(m):
+            code = m.group(1).strip()
+            args, kwargs = parse_args(code)
+            cmd, *args = args
+            # return f"<code>{cmd}: *{args}, **{kwargs}</code>"
+            return render(cmd, args, kwargs)
+
+        return super().paragraph(SHORTCODE.sub(shortcode, text))
 
 
 class ShortcodesPlugin(Plugin):
-    name = 'shortcodes'
-    description = u'Add your description here.'
+    name = "shortcodes"
+    description = "Embeds shortcodes in Markdown."
 
-    def on_process_template_context(self, context, **extra):
-        def test_function():
-            return 'Value from plugin %s' % self.name
-        context['test_function'] = test_function
+    def on_markdown_config(self, config, **extra):
+        config.renderer_mixins.append(ShortcodesMixin)
+
